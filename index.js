b153fe81b43d99cd99e44ebabe392047666af7f0
myrinja <myrinjau98@gmail.com>
2023-07-17 23:23:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+  function findMatching(drivers, name) {
+    const lowerCaseName = name.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseName);
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, targetName) {
+    return drivers.filter(driver => driver.name === targetName);
+  }
+  
+ 
